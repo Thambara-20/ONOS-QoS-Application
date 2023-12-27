6a6ff1bc4b8ccc7a8bf7eeccd233092e6da96d8d
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-27 18:24:02 +0530

    updated addqueue

--- QoS-app/src/main/java/org/foo/app/AppWebResource.java
+++ QoS-app/src/main/java/org/foo/app/AppWebResource.java
@@ -15,14 +15,25 @@
  */
 package org.foo.app;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.onosproject.net.behaviour.QueueDescription;
 import org.onosproject.rest.AbstractWebResource;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.onlab.util.Tools.nullIsNotFound;
 
@@ -32,30 +43,53 @@ import static org.onlab.util.Tools.nullIsNotFound;
 @Path("api")
 public class AppWebResource extends AbstractWebResource {
 
-    /**
-     * Get hello world greeting.
-     *
-     * @return 200 OK
-     */
+
+//    private final OvsdbQueueConfig ovsdbQueueConfig = new OvsdbQueueConfig();
+@POST
+@Path("addQueue")
+@Consumes("application/json")
+@Produces("application/json")
+public Response addQueue(InputStream stream) {
+
+    try {
+        ObjectNode responseNode = mapper().createObjectNode();
+
+        // Read JSON input
+        JsonNode input = mapper().readTree(stream);
+
+        // Validate JSON input
+        if (!input.has("deviceId") || !input.has("portNumber") || !input.has("queueDescription")) {
+            responseNode.put("error", "'deviceId', 'portNumber', and 'queueDescription' must be provided in the JSON input.");
+            return Response.status(Response.Status.BAD_REQUEST).entity(responseNode).build();
+        }
+
+        // Extract values from JSON input
+        String deviceId = input.get("deviceId").asText();
+        int portNumber = input.get("portNumber").asInt();
+        JsonNode queueDescriptionNode = input.get("queueDescription");
+
+
+
+
+
+        // Call the existing addQueue method from OvsdbQueueConfig
+//        boolean success = ovsdbQueueConfig.addQueue(queueDescription);
+
+        responseNode.put("success", true);
+        return Response.ok(responseNode).build();
+    } catch (Exception e) {
+        return Response.serverError().entity("Error processing JSON input: " + e.getMessage()).build();
+    }
+}
+
+
+
     @GET
     @Path("getQueue")
     public Response getQueue() {
-        ObjectNode node = mapper().createObjectNode().put("getQueue","getQueue");
+        ObjectNode node = mapper().createObjectNode().put("getQueue", "getQueue");
         return ok(node).build();
     }
 
-    @POST
-    @Path("updateQueue")
-    public Response updateQueue() {
-        ObjectNode node = mapper().createObjectNode().put("updateQueue","updateQueue");
-        return ok(node).build();
-    }
-
-    @DELETE
-    @Path("deleteQueue")
-    public Response deleteQueue() {
-        ObjectNode node = mapper().createObjectNode().put("deleteQueue","deleteQueue");
-        return ok(node).build();
-    }
 
 }
